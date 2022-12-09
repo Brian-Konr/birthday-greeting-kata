ca6c2b58e3b3f9287b59215b8c1f7c18f2c35e51
Brian Konr <b08705038@ntu.edu.tw>
2022-12-10 00:22:21 +0800

    use Npgsql package to make connection work and can successfully fetch data from postgresql

--- BirthdayGreetingKataService/BirthdayGreetingKataService/Controllers/MessageController.cs
+++ BirthdayGreetingKataService/BirthdayGreetingKataService/Controllers/MessageController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Npgsql;
+using System.Data;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,6 +11,15 @@ namespace BirthdayGreetingKataService.Controllers
     [ApiController]
     public class MessageController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+        private readonly string _postgresPassword;
+        public MessageController(IConfiguration configuration)
+        {
+            _configuration= configuration;
+            string secrectFilePath = _configuration.GetConnectionString("PostgreSecretFilePath");
+            _postgresPassword = LoadPassword(secrectFilePath);
+        }
+
         // GET: api/<MessageController>
         [HttpGet]
         public IEnumerable<string> Get(
@@ -15,7 +27,32 @@ namespace BirthdayGreetingKataService.Controllers
             [FromQuery(Name = "day")] int day
         )
         {
+            string query = @"
+                SELECT * 
+                FROM 
+                    members
+            ";
+
+            string sqlDataSource = _configuration.GetConnectionString("PostgresqlCon");
+            // replace {YOUR_PASSWORD} with real password
+            sqlDataSource = sqlDataSource.Replace($"{{YOUR_PASSWORD}}", _postgresPassword);
+            using (NpgsqlConnection connection = new NpgsqlConnection(sqlDataSource))
+            {
+                connection.Open();
+                using (NpgsqlCommand sqlCommand = new NpgsqlCommand(query, connection))
+                {
+                    NpgsqlDataReader dataReader = sqlCommand.ExecuteReader();
+                    DataTable dataTable = new DataTable();
+                    dataTable.Load(dataReader);
+                }
+            }
             return new string[] { "value1", "value2" };
+        }
+        
+        private string LoadPassword(string filePath)
+        {
+            string password = System.IO.File.ReadAllText(filePath);
+            return password;
         }
     }
 }
