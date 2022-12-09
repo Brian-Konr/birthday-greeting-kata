3a1194591040dc1a458a86a0a8e515abcac67d34
Brian Konr <b08705038@ntu.edu.tw>
2022-12-09 21:40:06 +0800

    initialize a message controller to handle HTTP request

---
+++ BirthdayGreetingKataService/BirthdayGreetingKataService/Controllers/MessageController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BirthdayGreetingKataService.Controllers
+{
+    [Route("api/message")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        // GET: api/<MessageController>
+        [HttpGet]
+        public IEnumerable<string> Get(
+            [FromQuery(Name = "month")] int month,
+            [FromQuery(Name = "day")] int day
+        )
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
