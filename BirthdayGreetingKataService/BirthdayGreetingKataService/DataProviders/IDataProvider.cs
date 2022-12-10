b3539f3d26ee7e1e8a979a4df2b02e710fe53e8a
Brian Konr <b08705038@ntu.edu.tw>
2022-12-11 01:17:09 +0800

    construct DataProvider interface for different implementation such as Postgresql, MongoDB, ...

---
+++ BirthdayGreetingKataService/BirthdayGreetingKataService/DataProviders/IDataProvider.cs
@@ -0,0 +1,16 @@
+﻿using BirthdayGreetingKataService.Models;
+
+namespace BirthdayGreetingKataService.DataProviders
+{
+    /// <summary>
+    /// 實作此 Interface 的 classes 都要能夠提供 query 資料，並以統一格式回傳
+    /// </summary>
+    public interface IDataProvider
+    {
+        List<Member> FilterMembersByDateofBirth(int month, int day);
+
+        List<Member> FilterMembersByGender(string gender);
+
+        List<Member> FilterMembersByElder(int threshold = 49);
+    }
+}
---
+++ BirthdayGreetingKataService/BirthdayGreetingKataService/DataProviders/PostgreSqlDataProvider.cs
@@ -0,0 +1,22 @@
+﻿using BirthdayGreetingKataService.Models;
+
+namespace BirthdayGreetingKataService.DataProviders
+{
+    public class PostgreSqlDataProvider : IDataProvider
+    {
+        public List<Member> FilterMembersByDateofBirth(int month, int day)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Member> FilterMembersByElder(int threshold = 49)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Member> FilterMembersByGender(string gender)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
