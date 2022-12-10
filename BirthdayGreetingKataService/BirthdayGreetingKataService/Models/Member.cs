7d03279480744bd49c6c751b92d61613aed38a1a
Brian Konr <b08705038@ntu.edu.tw>
2022-12-11 00:45:48 +0800

    create a class to store user data

---
+++ BirthdayGreetingKataService/BirthdayGreetingKataService/Models/Member.cs
@@ -0,0 +1,27 @@
+﻿namespace BirthdayGreetingKataService.Models
+{
+    /// <summary>
+    /// 每一個 Member 物件對應到資料庫的一筆 data row
+    /// </summary>
+    public class Member
+    {
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Gender { get; set; }
+
+        public DateTime DateofBirth { get; set; }
+
+        public string Email { get; set; }
+
+        public Member(string lastName, string firstName, string gender, DateTime dateofBirth, string email)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            Gender = gender;
+            DateofBirth = dateofBirth;
+            Email = email;
+        }
+    }
+}
