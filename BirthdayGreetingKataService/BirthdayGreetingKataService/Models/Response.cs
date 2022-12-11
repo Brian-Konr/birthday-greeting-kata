5f67c607065588a5d90499d7f4086f401b7c8d89
Brian Konr <b08705038@ntu.edu.tw>
2022-12-11 15:41:51 +0800

    create a response class

---
+++ BirthdayGreetingKataService/BirthdayGreetingKataService/Models/Response.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace BirthdayGreetingKataService.Models
+{
+    public class Response
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+    }
+}
