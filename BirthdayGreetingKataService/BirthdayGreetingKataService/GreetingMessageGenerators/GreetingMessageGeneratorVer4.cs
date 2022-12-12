8a1d9df76feb20038ece424f7dbacdfe2347f590
Brian Konr <b08705038@ntu.edu.tw>
2022-12-12 22:43:08 +0800

    create ver4 message generator class

---
+++ BirthdayGreetingKataService/BirthdayGreetingKataService/GreetingMessageGenerators/GreetingMessageGeneratorVer4.cs
@@ -0,0 +1,13 @@
+﻿using BirthdayGreetingKataService.Models;
+
+namespace BirthdayGreetingKataService.GreetingMessageGenerators
+{
+    public class GreetingMessageGeneratorVer4 : IGreetingMessageGenerator
+    {
+        public Response GenerateGreetingMessage(Member member)
+        {
+            string content = $"Happy birthday, dear {member.LastName}, {member.FirstName}!";
+            return new Response(Constants.MessageTitle, content);
+        }
+    }
+}
