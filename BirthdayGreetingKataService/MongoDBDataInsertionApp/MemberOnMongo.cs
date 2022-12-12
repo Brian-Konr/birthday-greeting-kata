8ae2b101794d1b523ad19fd893f99eddd043dfba
Brian Konr <b08705038@ntu.edu.tw>
2022-12-12 23:53:20 +0800

    create a console app for adding data into Mongo

---
+++ BirthdayGreetingKataService/MongoDBDataInsertionApp/MemberOnMongo.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBDataInsertionApp
+{
+    public class MemberOnMongo
+    { 
+        public ObjectId Id { get; set; }
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Gender { get; set; }
+
+        public DateTime DateofBirth { get; set; }
+
+        public string Email { get; set; }
+
+        public MemberOnMongo(ObjectId id, string lastName, string firstName, string gender, DateTime dateofBirth, string email)
+        {
+            Id = id;
+            LastName = lastName;
+            FirstName = firstName;
+            Gender = gender;
+            DateofBirth = dateofBirth;
+            Email = email;
+        }
+    }
+}
---
+++ BirthdayGreetingKataService/MongoDBDataInsertionApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using BirthdayGreetingKataService.DataProviders;
+using BirthdayGreetingKataService.Models;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+
+// Replace the uri string with your MongoDB deployment's connection string.
+string password = File.ReadAllText("C:\\Users\\user\\Documents\\GitHub\\birthday-greeting-kata\\BirthdayGreetingKataService\\BirthdayGreetingKataService\\mongodb_password.txt");
+var uri = $"mongodb+srv://briankonr:{password}@cluster0.bnnc5xp.mongodb.net/?retryWrites=true&w=majority";
+// instruct the driver to camelCase the fields in MongoDB
+var pack = new ConventionPack { new CamelCaseElementNameConvention() };
+ConventionRegistry.Register("elementNameConvention", pack, x => true);
+var client = new MongoClient(uri);
+
+// database and colletion code goes here
+var db = client.GetDatabase("test");
+var coll = db.GetCollection<Member>("members");
+
+
+var data = MockDataProvider.GenerateAllMembers();
+coll.InsertMany(data);
+
+Console.WriteLine("Data added successfully!...");
+
