83fd9c944908a51f8f3e00958477e2ff6a156df8
Brian Konr <b08705038@ntu.edu.tw>
2022-12-13 00:23:23 +0800

    working on MongoDBDataProvider implementation

---
+++ BirthdayGreetingKataService/BirthdayGreetingKataService/DataProviders/MongoDBDataProvider.cs
@@ -0,0 +1,27 @@
+﻿using BirthdayGreetingKataService.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace BirthdayGreetingKataService.DataProviders
+{
+    public class MongoDBDataProvider : IDataProvider
+    {
+        private readonly IMongoCollection<Member> _members;
+        
+        public MongoDBDataProvider(IConfiguration configuration) 
+        {
+            string connectionString = configuration.GetConnectionString("MongoDBCon");
+            string password = File.ReadAllText(configuration.GetConnectionString("MongoSecretFilePath"));
+            connectionString = connectionString.Replace($"{{YOUR_PASSWORD}}", password);
+
+            // create mongo client
+            MongoClient mongoClient = new MongoClient(connectionString);
+            IMongoDatabase database = mongoClient.GetDatabase("test");
+            _members = database.GetCollection<Member>("members");
+        }
+        public List<Member> FilterMembers(int? month, int? day, string? gender, bool? isElder)
+        {
+            return new List<Member>();
+        }
+    }
+}
