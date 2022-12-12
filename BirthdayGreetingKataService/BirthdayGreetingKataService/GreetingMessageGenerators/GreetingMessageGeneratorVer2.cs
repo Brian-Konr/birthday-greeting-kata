941d24307812aed2914f01837e430447222ae7d7
Brian Konr <b08705038@ntu.edu.tw>
2022-12-12 13:01:34 +0800

    create GreetingMessageGeneratorVer2

---
+++ BirthdayGreetingKataService/BirthdayGreetingKataService/GreetingMessageGenerators/GreetingMessageGeneratorVer2.cs
@@ -0,0 +1,25 @@
+﻿using BirthdayGreetingKataService.Models;
+
+namespace BirthdayGreetingKataService.GreetingMessageGenerators
+{
+    public class GreetingMessageGeneratorVer2 : IGreetingMessageGenerator
+    {
+        public Response GenerateGreetingMessage(Member member)
+        {
+            string content = string.Empty;
+            if (member.Gender == "Male")
+            {
+                content = $"Happy birthday, dear {member.FirstName}!\r\nWe offer special discount 20% off for the following items:\r\nWhite Wine, iPhone X\r\n";
+            }
+            else if (member.Gender == "Female")
+            {
+                content = $"Happy birthday, dear {member.FirstName}!\r\nWe offer special discount 50% off for the following items:\r\nCosmetic, LV Handbags\r\n";
+            }
+            else
+            {
+                content = $"Happy birthday, dear {member.FirstName}!";
+            }
+            return new Response(Constants.MessageTitle, content);
+        }
+    }
+}
