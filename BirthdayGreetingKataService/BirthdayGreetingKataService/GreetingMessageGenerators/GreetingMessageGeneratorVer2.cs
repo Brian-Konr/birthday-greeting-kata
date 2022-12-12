da4ee428c578a4aee14e03cb14a1b07a07f9fb25
Brian Konr <b08705038@ntu.edu.tw>
2022-12-12 13:03:58 +0800

    modify \r\n to \n

--- BirthdayGreetingKataService/BirthdayGreetingKataService/GreetingMessageGenerators/GreetingMessageGeneratorVer2.cs
+++ BirthdayGreetingKataService/BirthdayGreetingKataService/GreetingMessageGenerators/GreetingMessageGeneratorVer2.cs
@@ -9,11 +9,11 @@ namespace BirthdayGreetingKataService.GreetingMessageGenerators
             string content = string.Empty;
             if (member.Gender == "Male")
             {
-                content = $"Happy birthday, dear {member.FirstName}!\r\nWe offer special discount 20% off for the following items:\r\nWhite Wine, iPhone X\r\n";
+                content = $"Happy birthday, dear {member.FirstName}!\nWe offer special discount 20% off for the following items:\nWhite Wine, iPhone X";
             }
             else if (member.Gender == "Female")
             {
-                content = $"Happy birthday, dear {member.FirstName}!\r\nWe offer special discount 50% off for the following items:\r\nCosmetic, LV Handbags\r\n";
+                content = $"Happy birthday, dear {member.FirstName}!\nWe offer special discount 50% off for the following items:\nCosmetic, LV Handbags";
             }
             else
             {
