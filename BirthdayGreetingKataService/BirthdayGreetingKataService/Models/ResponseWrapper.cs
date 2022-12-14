7c9b0b761ef042501b747647c70078166a39711a
Brian Konr <b08705038@ntu.edu.tw>
2022-12-15 02:14:59 +0800

    create a wrapper class to wrap list of responses

---
+++ BirthdayGreetingKataService/BirthdayGreetingKataService/Models/ResponseWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Serialization;
+
+namespace BirthdayGreetingKataService.Models
+{
+    [XmlRoot("root")]
+    public class ResponseWrapper
+    {
+        [XmlArray("MessageList"), XmlArrayItem(typeof(Response), ElementName = "Message")]
+        public List<Response> Responses { get; set; }
+
+        public ResponseWrapper()
+        {
+            Responses = new List<Response>();
+        }
+        public ResponseWrapper(List<Response> responses)
+        {
+            Responses = responses;
+        }
+    }
+}
